ddfccd66ef405d1f6bc3c09194ee1f37b6267751
mrunal-mania <mrunal.mania97@gmail.com>
2024-02-19 20:32:16 -0700

    Before gradio stage

--- create_embeddings.py
+++ create_embeddings.py
@@ -3,5 +3,22 @@ import pickle
 import pandas as pd
 
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("sentence-transformers/all-mpnet-v2")
 
+with open("description.csv", 'r') as f:
+    lines = f.readlines()
+
+lines = [line.strip().split(",") for line in lines]
+
+for idx, line in enumerate(lines):
+    lines[idx] = [lines[0], ",".join(line[1:])]
+
+df = pd.DataFrame(lines, columns=['id', 'desc'])
+
+embeddings= model.encode(df['desc'].tolist(), show_progress_bar=True)
+
+with open('embeddings.pkl', 'wb') as f:
+    pickle.dump(embeddings, f)
+
+
+print(embeddings.shape)
